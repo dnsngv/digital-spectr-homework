8b09dff756e0c676cdfffedb7d7b519018e46eef
Denis Neganov <d.negannov@gmail.com>
2021-05-04 23:41:21 +0500

    week4 | Работа над ошибками (добавил бэкграуд страницы, селекторы для повторяющихся элементов, стрелки через псевдоэлемент), -ошибки в dev

--- week4-riter/js/slider.js
+++ week4-riter/js/slider.js
@@ -1,6 +1,6 @@
 /* Индекс слайда по умолчанию */
 var slideIndex = 1;
-showSlides(slideIndex);
+
 
 /* Устанавливает текущий слайд */
 function currentSlide(n) {
@@ -23,9 +23,13 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+    for (i = 0; i > slides.length; ) {
+        slides[i].style.display = "block";
+    }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
+
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 
